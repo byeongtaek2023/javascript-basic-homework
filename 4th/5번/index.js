da58c68ac937f07b3b1e6d5de22519a7fc2b339e
byeongtaek <respect21047463@gmail.com>
2023-11-14 13:46:16 +0900

    4회차 과제 완료

--- 4th/5번/index.js
+++ 4th/5번/index.js
@@ -1,3 +1,22 @@
 // 여기에 1번 문제의 답을 작성하세요.
+async function B() {
+  const data = await fetch("https://dummyjson.com/products");
+  const result = await data.json();
+  console.log(result);
+}
+B();
 
 // 여기에 2번 문제의 답을 작성하세요.
+async function F() {
+  const info = await fetch("https://dummyjson.com/products/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      title: "BMW Pencil",
+      /* other product data */
+    }),
+  });
+  const parse = await info.json();
+  console.log(parse);
+}
+F();
